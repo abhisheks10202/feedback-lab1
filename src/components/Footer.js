55c484be6266002976a129cd1f4941895662f3b9
Abhishek Singh <abhisheks8452@gmail.com>
2021-07-11 00:24:43 +0530

    lab 1 sem 2nd feedback

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <span>Copyright &copy; 2021 All Rights Reserved : Abhishek Singh</span>
+    </footer>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function header () {
+  return (
+    <header>
+      <div>FEEDBACK-LAB1</div>
+    </header>
+  )
+}
